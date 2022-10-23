ffcd48abc98cca5704a75a244bc4c3c92c232732
Adilcank23 <95485912+Adilcank23@users.noreply.github.com>
2022-10-23 18:35:08 +0300

    Add files via upload

---
+++ ödev 1-2/ödev 1-2/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Win32.SafeHandles;
+
+Console.WriteLine(48 / 6 / 4);
+Console.WriteLine(24 / 3 * 2);
+Console.WriteLine((7 + 4) * 2 - 1 + 8 / 2);
+Console.WriteLine((5 - 1) * 2 - 1 + 7 ^ 2 / 2);
+
+
