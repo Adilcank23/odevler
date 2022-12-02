0e5ca46798d1fe4cdc35b4425454541406c448d1
Adilcank23 <95485912+Adilcank23@users.noreply.github.com>
2022-12-02 17:30:12 +0300

    Add files via upload

---
+++ 3.hafta/Class2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.hafta
+{
+    internal class Class2
+    {
+        public void ornek7(string s)
+        {
+            for (s = Console.ReadLine(); s != "çıkış";
+                s = Console.ReadLine())
+                Console.WriteLine(s);
+        }
+        public void ornek8()
+        {
+            int i = 0, a, n;
+            Console.Write("bir sayı girin"); n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("artım miktarı"); a = Convert.ToInt32(Console.ReadLine());
+            for (; i < n;)
+            {
+                Console.Write("{0} ", i); i += a;
+
+            }
+
+        }
+        public void ornek9()
+        {
+            int k,t,toplam,n1,n2;
+            Console.Write("aralık başlangıcı :");n1= Convert.ToInt32(Console.ReadLine());
+            Console.Write("aralık sonu :"); n2 = Convert.ToInt32(Console.ReadLine());
+            if (n1 <= n2)
+            {
+                for (k = n1; k <= n2; k++)
+                {
+                    toplam = 0;
+                    for (t = 1; t <= k; t++) { if (k % t == 0) toplam += t; }
+                    if (toplam == k + 1) Console.WriteLine(k);
+                }
+            }
+            else
+                Console.WriteLine("geçerli bir aralık giriniz"  );
+        }
+
+        public void ornek10()
+        {
+            int sayi;
+            Console.WriteLine("bir tamsayı girin");
+            sayi= Convert.ToInt32(Console.ReadLine());
+
+        for(int bit = 32; bit >= 1; bit--)
+            {
+                Console.Write("{0}", (sayi >> bit - 1) & 1);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 3.hafta/Program.cs
@@ -0,0 +1,61 @@
+﻿        int a = 2; int b = 3; int c = 6; int d = 1;
+        Console.WriteLine("(a<b)&&(c<d) -->" + ((a < b) && (c < d)));
+        Console.WriteLine("(a<b)||(c<d) -->" + ((a < b) || (c < d)));
+        Console.WriteLine("! (a<b) -->" + (!(a < b)));
+        Console.WriteLine("(a<b)&(c<d) -->" + ((a < b) & (c < d)));
+        Console.WriteLine("(a<b)|(c<d) -->" + ((a < b) | (c < d)));
+        Console.WriteLine("(a<b)^(c<d) -->" + ((a < b) ^ (c < d)));
+    
+    Console.WriteLine("----------------------------------------");
+    a = 2; b = 3; c = 6;
+    Console.WriteLine(" (a & b) --> " + (a & b));
+    Console.WriteLine(" (a | b) --> " + (a | b));
+    Console.WriteLine(" (a ^ b) --> " + (a ^ b));
+    Console.WriteLine(" ( ~a ) --> " + (~a));
+    Console.WriteLine(" ( ~b ) --> " + (~b));
+    Console.WriteLine(" ( ~c ) --> " + (~c));
+    Console.WriteLine("----------------------------------------");
+    int i = 0;
+    while (i < 5)
+    {
+        Console.WriteLine("i = " + i);
+        i++;
+    }
+    Console.WriteLine("----------------------------------------");
+    i = 0;
+    do
+    {
+        Console.WriteLine("i = " + i);
+        i++;
+    } while (i < 0);
+    Console.WriteLine("----------------------------------------");
+    int secim;
+    do
+    {
+        Console.WriteLine("işlemler\n========");
+        Console.WriteLine("1 - Toplama");
+        Console.WriteLine("2 - çıkarma");
+        Console.WriteLine("3 - çarpma");
+        Console.WriteLine("4 - bölme");
+        Console.WriteLine("0 - çıkış\n");
+        Console.WriteLine("işleminizi seçin:");
+        secim = int.Parse(Console.ReadLine());
+        switch (secim)
+        {
+            case 1: Console.WriteLine("toplama"); break;
+            case 2: Console.WriteLine("çıkarma"); break;
+            case 3: Console.WriteLine("çarpma"); break;
+            case 4: Console.WriteLine("bölme"); break;
+            case 0: Console.WriteLine("çıkış"); break;
+            default: Console.WriteLine("yanlış seçim"); break;
+        }
+    }
+    while (secim != 0);
+    Console.WriteLine("----------------------------------------");
+    i = 0;
+    for (int j = 0; i < 20; i++, j++)
+    {
+        i *= j;
+        Console.WriteLine("i = " + i + " j = " + j);
+    }
+}
